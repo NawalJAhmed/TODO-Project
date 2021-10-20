6994e89d4efaa3d734a4c705152a0fc83c21b12e
NawalJAhmed <11577850+NawalJAhmed@users.noreply.github.com>
2021-10-20 12:35:37 -0400

    added stuff for tasks

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,18 @@
 window.addEventListener("load", (event)=>{
     console.log("hello from javascript!")
 })
+
+const buttons = document.querySelectorAll('.delete-btn')
+
+for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.addEventListener('click', async (e) => {
+        e.preventDefault();
+        const postId = e.target.id.split('-')[1]
+        const res = await fetch(`/posts/${postId}`, {
+            method: 'DELETE'
+        })
+
+        
+    })
+}
