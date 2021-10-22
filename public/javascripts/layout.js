28dc195dda353915bb17054f536fecbd8fc3956c
will-short <willofmetal85@gmail.com>
2021-10-22 16:05:34 -0500

    fixed highlighting

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -5,7 +5,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
     let groups = document.querySelectorAll(".groupButton > div > a");
     let links = [dash, ...groups];
     links.forEach((link) => {
-      if (link.href === window.location.href) link.style.color = "white";
+      if (window.location.href.startsWith(link.href))
+        link.style.color = "white";
     });
 
     let groupHeader = document.querySelector(".groupTop");
